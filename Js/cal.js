cb776027409c735c441629a43bca2a3a577576ae
Nikhilch123 <108396717+Nikhilch123@users.noreply.github.com>
2022-08-22 16:12:18 +0530

    Calculator

---
+++ Js/cal.js
@@ -0,0 +1,54 @@
+// All the buttons tags    stored in one veriabls(buttons)
+
+let buttons =document.getElementsByTagName('button');
+// result stored in result variable
+
+let result = document.getElementById('result');
+
+
+
+for(i=0;i<buttons.length;i++){
+    buttons[i].onclick = function(){
+        let input = this.innerText;
+        updateDisplay(input)
+        
+    }
+}
+
+function updateDisplay(input){
+    let currentDisplay = result.innerText
+    if(currentDisplay=="0"){
+        if(input!="C"&&input!="DEL" && input!="="){
+            result.innerText=""
+            result.innerText=input
+        }
+
+    }
+
+    
+   else{
+       
+    
+        if(input=="DEL"){
+            result.innerText = currentDisplay.substring(0,currentDisplay.length-1)
+            
+        }
+        if(input=="C"){
+            result.innerText="0"
+        }
+        if(input!="C"&&input!="DEL"&&input!="="){
+            result.innerText+=input
+            x = x+6
+            x+=7
+        }
+
+        // To evaluvate te experssion
+
+        if(input=="="){          
+            let display = result.innerText
+            result.innerText= eval(display)
+        }
+
+    
+}
+}
